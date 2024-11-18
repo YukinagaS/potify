de387203fda39bf48ed402f86242a932619c512a
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2024-11-18 16:08:14 +0900

    install devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Custom default url options
+  config.action_mailer.default_url_options = { host: 'https://potify-9fffc7477e26.herokuapp.com/', port: 3000 }
 end
