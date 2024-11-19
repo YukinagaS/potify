871c6ac57b9e8700740c512e0f86f5a67cb7eaf3
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2024-11-19 15:16:24 +0900

    added comprehensive view on user show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,2 @@
 class BookingsController < ApplicationController
-  def index # users/:id/bookings
-    @user = User.find(params[:user_id])
-    @bookings = Booking.where(user: @user)
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+ def show
+  @user = current_user
+  @toilets = @user.toilets
+  @toilet_bookings = Booking.where(toilet_id: @toilets)
+  @bookings = @user.bookings
+ end
 end
