c124ce74a72d7626d006df7a3085b789796c10a7
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2024-11-19 10:51:25 +0900

    install cloudinary

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -2,4 +2,10 @@ class ToiletsController < ApplicationController
   def index
     @toilets = Toilet.all
   end
+
+  private
+
+  def toilet_params
+    params.require(:toilet).permit(:name, :price, :address, :description, :photo)
+  end
 end
