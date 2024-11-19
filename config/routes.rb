c4f67d96bf109024b0dd4b6ad7a450dea81b5ec9
Arisa Segawa <arisa.segawa@gmail.com>
2024-11-19 15:49:52 +0900

    fixed routes again

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:create]
   end
 
-  resources :bookings, only: [:index, :destroy]
-
-  resources :users, only: [:show] do
-    resources :bookings, only: [:index, :update]
-  end
+  resources :bookings, only: [:index, :destroy, :update]
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
