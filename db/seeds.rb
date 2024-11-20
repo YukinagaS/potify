341a299d1b661a728ff6390466675ab38a60a287
Arisa Segawa <arisa.segawa@gmail.com>
2024-11-20 20:14:03 +0900

    changed has one photo to has_many, added a carousel for toilets with multiple photos, added the photos to the show page but need to work on style

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,41 +23,53 @@ puts "building toilets..."
 
 celso_t = Toilet.create!(name: "The Duchess", price: 50, address: "52 Bowl Dr.", description: "status: unprocessable_entity", user: celso)
 file = File.open("app/assets/images/horse_toilet.jpg")
-celso_t.photo.attach(io: file, filename: "toilet.jpg", content_type: "image/jpg")
+celso_t.photos.attach(io: file, filename: "toilet.jpg", content_type: "image/jpg")
+celso_t.save
+file = File.open("app/assets/images/toilet_paper.jpg")
+celso_t.photos.attach(io: file, filename: "toilet_paper.jpg", content_type: "image/jpg")
 celso_t.save
 
 doug_t = Toilet.create(name: "Grandma's Party", price: 100, address: "who cares", description: "Ideal for couples", user: doug)
 file = File.open("app/assets/images/couple_toilet.jpg")
-doug_t.photo.attach(io: file, filename: "couple_toilet.jpg", content_type: "image/jpg")
+doug_t.photos.attach(io: file, filename: "couple_toilet.jpg", content_type: "image/jpg")
 doug_t.save
 
 yann_t = Toilet.create(name: "The Alisa", price: 1, address: "an alleyway", description: "don't use this one", user: yann)
 file = File.open("app/assets/images/golden_toilet.webp")
-yann_t.photo.attach(io: file, filename: "golden_toilet.webp", content_type: "image/webp")
+yann_t.photos.attach(io: file, filename: "golden_toilet.webp", content_type: "image/webp")
+yann_t.save
+file = File.open("app/assets/images/gold-plated-toilet.webp")
+yann_t.photos.attach(io: file, filename: "golden_toilet.webp", content_type: "image/webp")
+yann_t.save
+file = File.open("app/assets/images/golden1.png")
+yann_t.photos.attach(io: file, filename: "golden_toilet.webp", content_type: "image/webp")
+yann_t.save
+file = File.open("app/assets/images/gold-plated-toilet.webp")
+yann_t.photos.attach(io: file, filename: "golden2.png", content_type: "image/webp")
 yann_t.save
 
 
 stuart_t = Toilet.create!(name: "Hello Kitty", price: 50, address: "52 Bowl Dr.", description: "strong params", user: celso)
 file = File.open("app/assets/images/scary.jpg")
-stuart_t.photo.attach(io: file, filename: "scary.jpg", content_type: "image/jpg")
+stuart_t.photos.attach(io: file, filename: "scary.jpg", content_type: "image/jpg")
 stuart_t.save
 
 
 alisa_t = Toilet.create(name: "Cool Biz", price: 100, address: "who cares", description: "for relaxing times", user: doug)
 file = File.open("app/assets/images/chains.jpg")
-alisa_t.photo.attach(io: file, filename: "chains.jpg", content_type: "image/jpg")
+alisa_t.photos.attach(io: file, filename: "chains.jpg", content_type: "image/jpg")
 alisa_t.save
 
 
 denis_t = Toilet.create(name: "Captain AND Tenille", price: 1, address: "hell", description: "good god, you're going to have fun", user: yann)
 file = File.open("app/assets/images/BAR.jpg")
-denis_t.photo.attach(io: file, filename: "BAR.jpg", content_type: "image/jpg")
+denis_t.photos.attach(io: file, filename: "BAR.jpg", content_type: "image/jpg")
 denis_t.save
 
 12.times do |index|
   toilet = Toilet.create!(name: "#{Faker::FunnyName.name} #{index}", price: (500..2000).to_a.sample, address: "#{Faker::Address.street_address}, #{Faker::Address.city} #{index}", description: "#{Faker::Quote.jack_handey} #{index}", user: [yann, stuart, denis, alisa, doug, celso].sample)
   file = File.open(["app/assets/images/BAR.jpg", "app/assets/images/01.jpg", "app/assets/images/02.webp", "app/assets/images/03.jpg", "app/assets/images/04.jpeg", "app/assets/images/05.webp", "app/assets/images/06.jpg", "app/assets/images/07.jpg", "app/assets/images/08.jpg"].sample)
-  toilet.photo.attach(io: file, filename: "BAR.jpg", content_type: "image/jpg")
+  toilet.photos.attach(io: file, filename: "BAR.jpg", content_type: "image/jpg")
   toilet.save
 end
 
