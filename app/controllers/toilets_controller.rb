549e2aa36a9090d4a95fe9ff4b4eb93e928e9bd9
Denis Sigal <182473309+densigal@users.noreply.github.com>
2024-11-20 14:20:37 +0900

    partial ongoing

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -5,7 +5,8 @@ class ToiletsController < ApplicationController
     @markers = @flats.geocoded.map do |flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {flat: flat})
       }
     end
   end
