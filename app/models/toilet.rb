96b02a88c83799ce5ab23fc28fcd1ac46ebe3b2b
Denis Sigal <182473309+densigal@users.noreply.github.com>
2024-11-20 12:44:34 +0900

    map implemented

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -8,4 +8,7 @@ class Toilet < ApplicationRecord
   validates :price, presence: true # may require more limitations
   validates :address, presence: true
   validates :description, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
