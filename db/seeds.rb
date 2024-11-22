2ceed3b0bee2075a14d2f31cb1b9f80662fda137
Denis Sigal <182473309+densigal@users.noreply.github.com>
2024-11-22 16:02:16 +0900

    changed location in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,7 +43,7 @@ puts "there are #{User.count} users"
 
 puts "building toilets..."
 
-celso_t = Toilet.create!(name: "The Duchess", price: 50, address: "52 Bowl Dr.", short_description: "The Duchess is a horse-shaped toilet with glossy porcelain and antique gold accents, blending art, luxury, and whimsy for a regal bathroom statement.", description: "The Duchess is a horse-shaped toilet crafted from glossy porcelain with antique gold accents. The horse’s head forms the tank, while the flowing mane and tail add elegance and stability.
+celso_t = Toilet.create!(name: "The Duchess", price: 50, address: "3-chōme-2 Takaban, Meguro City, Tokyo 152-0004", short_description: "The Duchess is a horse-shaped toilet with glossy porcelain and antique gold accents, blending art, luxury, and whimsy for a regal bathroom statement.", description: "The Duchess is a horse-shaped toilet crafted from glossy porcelain with antique gold accents. The horse’s head forms the tank, while the flowing mane and tail add elegance and stability.
 
 The integrated seat mirrors the horse’s back for comfort, and a playful mirror at the bottom offers a unique twist.
 
@@ -61,12 +61,12 @@ file = File.open("app/assets/images/horse.png")
 celso_t.photos.attach(io: file, filename: "horse.png", content_type: "image/png")
 celso_t.save
 
-doug_t = Toilet.create(name: "Grandma's Party", price: 100, address: "who cares", short_description: "Ideal for couples" , description: "Ideal for couples", user: doug)
+doug_t = Toilet.create(name: "Grandma's Party", price: 100, address: "2 Chome-6-14 Megurohoncho, Meguro City, Tokyo 152-0002", short_description: "Ideal for couples" , description: "Ideal for couples", user: doug)
 file = File.open("app/assets/images/couple_toilet.jpg")
 doug_t.photos.attach(io: file, filename: "couple_toilet.jpg", content_type: "image/jpg")
 doug_t.save
 
-yann_t = Toilet.create(name: "The Golden Throne", price: 1, address: "an alleyway", short_description: "Experience luxury with our Golden & Marble Toilet, blending timeless marble elegance with rich gold accents for a sophisticated bathroom centerpiece." , description: "This luxurious Golden and Marble Elegant Toilet combines the timeless beauty of marble with the opulence of gold accents. The smooth marble surface and rich gold fixtures create a stunning contrast, adding sophistication and grandeur to any bathroom. Perfect for upscale spaces, it offers both elegance and modern functionality for an unforgettable bathroom experience.", user: yann)
+yann_t = Toilet.create(name: "The Golden Throne", price: 1, address: "2 Chome-11-3 Meguro, Meguro City, Tokyo 153-0063", short_description: "Experience luxury with our Golden & Marble Toilet, blending timeless marble elegance with rich gold accents for a sophisticated bathroom centerpiece." , description: "This luxurious Golden and Marble Elegant Toilet combines the timeless beauty of marble with the opulence of gold accents. The smooth marble surface and rich gold fixtures create a stunning contrast, adding sophistication and grandeur to any bathroom. Perfect for upscale spaces, it offers both elegance and modern functionality for an unforgettable bathroom experience.", user: yann)
 file = File.open("app/assets/images/golden_toilet.webp")
 yann_t.photos.attach(io: file, filename: "golden_toilet.webp", content_type: "image/webp")
 yann_t.save
@@ -80,7 +80,7 @@ file = File.open("app/assets/images/golden2.png")
 yann_t.photos.attach(io: file, filename: "golden2.png", content_type: "image/png")
 yann_t.save
 
-will_t = Toilet.create(name: "The Cadillac", price: 800, address: "an alleyway", short_description: "The Cadillac Toilet combines sleek style, plush seating, and efficient performance, offering automotive elegance and first-class comfort in your bathroom." , description: "Experience the perfect blend of style and comfort with the Cadillac Toilet. Inspired by the sleek lines and elegance of a Cadillac, this toilet offers plush seating and smooth, efficient performance. A true statement piece for those who appreciate luxury, it brings automotive flair and first-class comfort to your bathroom.", user: will)
+will_t = Toilet.create(name: "The Cadillac", price: 800, address: "5 Chome-12-6 Shirokanedai, Minato City, Tokyo 108-0071", short_description: "The Cadillac Toilet combines sleek style, plush seating, and efficient performance, offering automotive elegance and first-class comfort in your bathroom." , description: "Experience the perfect blend of style and comfort with the Cadillac Toilet. Inspired by the sleek lines and elegance of a Cadillac, this toilet offers plush seating and smooth, efficient performance. A true statement piece for those who appreciate luxury, it brings automotive flair and first-class comfort to your bathroom.", user: will)
 file = File.open("app/assets/images/cadillac2.png")
 will_t.photos.attach(io: file, filename: "golden_toilet.png", content_type: "image/webp")
 will_t.save
@@ -92,19 +92,19 @@ will_t.photos.attach(io: file, filename: "golden_toilet.webp", content_type: "im
 will_t.save
 
 
-stuart_t = Toilet.create!(name: "Hello Kitty", price: 50, address: "52 Bowl Dr.", short_description: "strong params", description: "strong params", user: celso)
+stuart_t = Toilet.create!(name: "Hello Kitty", price: 50, address: "153-0063 Tokyo, Meguro City, Meguro, 1 Chome−4−16 目黒Gビル 1F", short_description: "strong params", description: "strong params", user: celso)
 file = File.open("app/assets/images/scary.jpg")
 stuart_t.photos.attach(io: file, filename: "scary.jpg", content_type: "image/jpg")
 stuart_t.save
 
 
-alisa_t = Toilet.create(name: "Cool Biz", price: 100, address: "who cares", short_description: "for relaxing times", description: "for relaxing times", user: doug)
+alisa_t = Toilet.create(name: "Cool Biz", price: 100, address: "153-0064 Tokyo, Meguro City, Shimomeguro, 2 Chome−2−10 The Edge目黒 402", short_description: "for relaxing times", description: "for relaxing times", user: doug)
 file = File.open("app/assets/images/chains.jpg")
 alisa_t.photos.attach(io: file, filename: "chains.jpg", content_type: "image/jpg")
 alisa_t.save
 
 
-denis_t = Toilet.create(name: "Captain AND Tenille", price: 1, address: "hell", short_description: "good god, you're going to have fun", description: "good god, you're going to have fun", user: yann)
+denis_t = Toilet.create(name: "Captain AND Tenille", price: 1, address: "153-0064 Tokyo, Meguro City, Shimomeguro, 2 Chome−21−28 セントヒルズ目黒 2F", short_description: "good god, you're going to have fun", description: "good god, you're going to have fun", user: yann)
 file = File.open("app/assets/images/scary.jpg")
 denis_t.photos.attach(io: file, filename: "scary.jpg", content_type: "image/jpg")
 denis_t.save
